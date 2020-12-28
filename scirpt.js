dbe1ae08c478e87b91791038d3a95944be0999bd
shofikspace <mohammadshofik@gmail.com>
2020-12-28 10:54:45 +0200

    Navbar completed

---
+++ scirpt.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+  $(window).scroll(function(){
+    if(this.scrollY > 20){
+      $('.navbar').addClass("sticky");
+    }else{
+      $('.navbar').removeClass("sticky")
+    }
+  })
+});
+
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
